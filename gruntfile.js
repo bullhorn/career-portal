0e36ba1928c84df06ae25a5055df4fc9a9757d6f
Brian Kimball <bkimball@bullhorn.com>
2015-04-01 11:06:47 -0400

    Build system added with livereload

---
+++ gruntfile.js
@@ -0,0 +1,62 @@
+module.exports = function (grunt) {
+    require('load-grunt-tasks')(grunt);
+    
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        // Linting Options
+        jshint: {
+            files: ['gruntfile.js', 'resources/js/**/*.js'],
+            options: {
+                asi: true,
+                expr: true,
+                sub: true,
+                loopfunc: true
+            }
+        },
+        
+		sass: {
+			dist: {
+			  files: {
+				'resources/css/main.css': 'resources/scss/main.scss'
+			  }
+			}
+		  },
+		watch: {
+			styles: {
+				files: ['resources/**/*.scss'],
+				tasks: ['sass'],
+				options: {
+				  livereload: true
+				}
+			}
+		},
+        connect: {
+			server: {
+			  options: {
+				port: 9001,
+				hostname: 'localhost',
+                livereload: 35729
+			  }
+			}
+		},
+		
+        karma: {
+            unit: {
+                configFile: 'test/karma.conf.js',
+				singleRun: true
+            }
+        }
+    });
+
+    grunt.registerTask("default", [
+        'jshint',
+        'sass'
+    ]);
+
+    grunt.registerTask('serve', [
+		'default',
+		'connect',
+		'watch'
+	]);
+};
