e4b26cda473cc894fdabcf6027a0872752a83015
Joshua Godi <jgodi@bullhorn.com>
2015-08-06 07:44:06 -0500

    MISC: Using AngularDeferredBootstrap to load the app.json config file on runtime rather then inside an ES6 import. Updating the tests to provide this as well.

--- test/unit/services/apply.service.spec.js
+++ test/unit/services/apply.service.spec.js
@@ -1,7 +1,40 @@
 'use strict';
 
 describe('Service: ApplyService', function () {
-    beforeEach(module('CareerPortal'));
+    beforeEach(function () {
+        // Mock configuration
+        module(function ($provide) {
+            $provide.constant('configuration', {
+                "acceptedResumeTypes": [
+                    "html",
+                    "text",
+                    "txt",
+                    "pdf",
+                    "doc",
+                    "docx",
+                    "rtf",
+                    "odt"
+                ],
+                "companyName": "Karma Company",
+                "defaultLocale": "en-US",
+                "localeSupported": [
+                    "en-US",
+                    "fr-FR"
+                ],
+                "minUploadSize": 1024,
+                "maxRelatedJobs": 5,
+                "maxUploadSize": 204800,
+                "service": {
+                    "batchSize": 500,
+                    "corpToken": "1hs",
+                    "port": 8181,
+                    "swimlane": "-qa"
+                }
+            });
+        });
+
+        module('CareerPortal');
+    });
 
     it('should be defined', function () {
         return true;
