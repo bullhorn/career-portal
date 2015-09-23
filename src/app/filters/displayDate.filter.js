84b1ecf2ed92003dfd088b9392ace72ff4dc087b
Joshua Godi <jgodi@bullhorn.com>
2015-09-23 14:37:50 -0500

    chore(localization): Adding support for other locales too

--- src/app/filters/displayDate.filter.js
+++ src/app/filters/displayDate.filter.js
@@ -6,7 +6,7 @@ export default function (configuration, moment) {
         var now = moment();
 
         if (now.diff(momentDate, 'days') > 1) {
-            if (configuration.defaultLocale === 'en-EU') {
+            if (['en-EU', 'en-GB'].indexOf(configuration.defaultLocale) !== -1) {
                 return momentDate.format('DD/MM/YY');
             } else {
                 return momentDate.format('MM/DD/YY');
