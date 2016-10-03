c815252eb1a90fdbf27f71f67330074232761976
Ryan Dougherty <ryan.dougherty@bullhorn.com>
2016-10-03 09:34:35 -0400

    fixes for apply button behavior; bind ethnicity array to apply service form

--- src/app/services/eeoc.service.js
+++ src/app/services/eeoc.service.js
@@ -7,10 +7,32 @@ class EeocService {
         this.genderRaceEthnicity = !!configuration.eeoc && configuration.eeoc.genderRaceEthnicity === true;
         this.veteran = !!configuration.eeoc && configuration.eeoc.veteran === true;
         this.disability = !!configuration.eeoc && configuration.eeoc.disability === true;
+
+        this.selectedEthnicities = {};
     }
 
     isGenderRaceEthnicityEnabled() {
         return this.genderRaceEthnicity;
+    }
+
+    isEthnicityChecked() {
+
+        var checked = this.getCheckedEthnicities();
+
+        return checked.length > 0;
+    }
+
+    getCheckedEthnicities() {
+
+        var checked = [];
+
+        angular.forEach(this.selectedEthnicities, function (value, key) {
+            if (!!value) {
+                checked.push(key);
+            }
+        });
+
+        return checked;
     }
 
     isVeteranEnabled() {
