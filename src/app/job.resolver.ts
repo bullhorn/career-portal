26d8e32b82584e633d81cf8b1625585513602f31
Charles Barnes <charlesabarnesjr@gmail.com>
2019-05-19 06:53:10 -0500

    add job resolver

---
+++ src/app/job.resolver.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+import { Resolve } from '@angular/router';
+
+import { ActivatedRouteSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+import { SearchService } from './services/search/search.service';
+
+@Injectable()
+export class JobResolver implements Resolve<any> {
+  constructor(private searchService: SearchService) { }
+
+  public resolve(route: ActivatedRouteSnapshot): Observable<any> {
+    return this.searchService.openJob(route.paramMap.get('id'));
+  }
+}
