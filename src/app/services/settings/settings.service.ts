f48ccece285089ac6ba04e93322d5b04276424fb
Charles Barnes <cbarnes@bullhorn.com>
2019-10-22 16:45:28 -0500

    set translateservice location

--- src/app/services/settings/settings.service.ts
+++ src/app/services/settings/settings.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { isPlatformServer } from '@angular/common';
+import { TranslateService } from 'chomsky';
 
 const APP_CONFIG_URL: any = './app.json';
 
@@ -9,19 +10,24 @@ export class SettingsService {
 
   public static settings: ISettings;
   public static isServer: boolean;
+  public static urlRoot: string;
 
   constructor(private http: HttpClient, @Inject(PLATFORM_ID) platformId: string) {
     SettingsService.isServer = isPlatformServer(platformId);
   }
 
-  public load(): Promise<any> {
-    let request: Promise<any> = this.http.get(APP_CONFIG_URL).toPromise();
-    request.then(this.setConfig.bind(this));
-
-    return request;
+  public async load(): Promise<any> {
+    let data: any | ISettings = await this.http.get(APP_CONFIG_URL).toPromise();
+    this.setConfig(data);
   }
 
-  public setConfig(data: ISettings): void {
+  public async setConfig(data: ISettings): Promise<any> {
     SettingsService.settings = data;
+    if (SettingsService.urlRoot) {
+      TranslateService.setLocation(`${SettingsService.urlRoot}i18n`);
+    }
+    await TranslateService.use('english').toPromise();
+
   }
+
 }
