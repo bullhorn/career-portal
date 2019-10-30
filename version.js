ae6d8f5220043a70c76977a93acf33796763d3a0
Charles Barnes <cbarnes@bullhorn.com>
2019-10-30 07:04:36 -0500

    add version file generation

---
+++ version.js
@@ -0,0 +1,36 @@
+// Vendor
+const fs = require('fs');
+const path = require('path');
+const chalk = require('chalk');
+const JenkinsBuildNumber = process.argv['ji'] || 'UNKNOWN';
+const CommitHash = process.argv['gi'] || 'UNKNOWN';
+const now = new Date();
+
+let data = `
+_________                                           __________                   __           .__
+\_   ___ \ _____   _______   ____    ____  _______  \______   \  ____  _______ _/  |_ _____   |  |
+/    \  \/ \__  \  \_  __ \_/ __ \ _/ __ \ \_  __ \  |     ___/ /  _ \ \_  __ \\   __\\__  \  |  |
+\     \____ / __ \_ |  | \/\  ___/ \  ___/  |  | \/  |    |    (  <_> ) |  | \/ |  |   / __ \_|  |__
+ \______  /(____  / |__|    \___  > \___  > |__|     |____|     \____/  |__|    |__|  (____  /|____/
+        \/      \/              \/      \/                                                 \/
+
+Date: ${now.toUTCString()}
+Career Portal Version: ${require('./package.json').version}
+Build Information:
+  Build#: ${JenkinsBuildNumber}
+  Commit: ${CommitHash}
+Dependency Information:
+  NovoElements: ${require('./node_modules/novo-elements/package.json').version}
+  Chomsky: ${require('./node_modules/chomsky/package.json').version}
+`;
+
+console.log('Writing Version.txt');
+console.log(data);
+
+fs.writeFile(path.resolve('src', 'version.txt'), data, err => {
+  if (err) {
+    console.error('Failed to write version:', err.message);
+  } else {
+    console.log(chalk.blue('Version file written!'));
+  }
+});
