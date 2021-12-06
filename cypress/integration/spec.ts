4f142fb3075e7b210df7fc450a38f38e8793231a
Charles Barnes <cbarnes@bullhorn.com>
2021-12-06 08:09:53 -0600

    add automation to handle category selection

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -6,13 +6,45 @@ describe('Job List', () => {
     })
   })
   it('Filter Categories', () => {
-    cy.visit('/')
+    cy.intercept({
+      hostname: 'public-rest91.bullhornstaffing.com',
+      method: 'GET',
+      pathname: '/rest-services/1VCNF4/query/JobBoardPost',
+      query: {
+        fields: 'publishedCategory(id,name),count(id)',
+        orderBy: 'publishedCategory.name',
+        count: '500'
+      }
+    }).as('getCategories');
+    cy.visit('/');
+    cy.wait('@getCategories', {timeout: 30000});
+
     cy.get('app-sidebar [data-automation-id="category"] novo-check-list>div.check-box-group').each(($el, index, $list)=>{
       if (index < 10) {
         const automationId = $el.attr('data-automation-id');
-        expect(!!automationId).eq(true, 'empty category in list')      ;
-        $
-        $el.trigger('click');
+        expect(!!automationId).eq(true, 'empty category in list of categories');
+
+        cy.intercept({
+          hostname: 'public-rest91.bullhornstaffing.com',
+          method: 'GET',
+          pathname: '/rest-services/1VCNF4/search/JobOrder',
+          query: {
+            fields: 'id,title,publishedCategory(id,name),address(city,state,zip),employmentType,dateLastPublished,publicDescription,isOpen,isPublic,isDeleted,publishedZip,salary,salaryUnit',
+            count: '30',
+          }
+        }).as('getJobs');
+
+        cy.get(`[data-automation-id="${automationId}"]`).first().click();
+        cy.wait('@getCategories', {timeout: 30000});
+        cy.wait('@getJobs', {timeout: 30000});
+        cy.get('novo-list>div.job-card').each(($el, index, $list)=>{
+          const categoryCountRegex = /\(([^)]+)\)/;
+          const count = parseInt(categoryCountRegex.exec(automationId)[1]);
+          expect($list.length, 'The correct number of results for the specified category is not showing').equals(count);
+        });
+        cy.get('.bhi-checkbox-filled').click();
+        cy.wait('@getCategories', {timeout: 30000});
+        cy.wait(3000);
       } else {
         return false;
       }
